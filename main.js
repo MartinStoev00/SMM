f38aef91eabf51a16ba94315a123fd5c66feefe3
MartinStoev00 <martinstoev00@gmail.com>
2020-08-23 12:24:26 +0300

    sidenav improved

--- main.js
+++ main.js
@@ -1,53 +1,30 @@
-let bgSign = "BG"
-let enSign = "EN"
-let langBtn = document.getElementsByClassName("header__lang__btn")[0]
-let langBtnArrow = langBtn.getElementsByTagName("i")[0]
-let langDropdown = document.getElementsByClassName("header__lang__dropdown")[0]
+let selectedArea = innerWidth < 768 ? "sidenav" : "header";
 
+let langBtn = document.getElementsByClassName(`${selectedArea}__lang__btn`)[0]
+let langDropdown = document.getElementsByClassName(`${selectedArea}__lang__dropdown`)[0]
+let langBtnArrow = langBtn.getElementsByTagName("i")[0]
+let btns = document.getElementsByClassName(selectedArea)[0].querySelectorAll("[data-btn]");
 let headerHeightWindow = () => {
     let headerH = document.getElementsByClassName("header")[0].getBoundingClientRect().height;
     document.getElementsByClassName("header__placeholder")[0].style.height = (headerH) + "px";
 }
-let btns = document.querySelectorAll("[data-btn]");
-let bg = document.querySelectorAll('[data-lang="bg"]')
-let en = document.querySelectorAll('[data-lang="en"]')
-let hideEn = () => {
-    en.forEach((enBlock) => {
-        enBlock.style.display = "none";
+let showSelect = (input) => {
+    document.querySelectorAll('[data-lang]').forEach((block) => {
+        block.style.display = "none";
     })
-    bg.forEach((bgBlock) => {
-        bgBlock.style.display = "inline";
+    document.querySelectorAll(`[data-lang="${input.replace("#", "").toLowerCase()}"]`).forEach((block) => {
+        block.style.display = "inline";
     })
-}
-let hideBg = () => {
-    en.forEach((enBlock) => {
-        enBlock.style.display = "inline";
-    })
-    bg.forEach((bgBlock) => {
-        bgBlock.style.display = "none";
-    })
-}
-if(window.location.hash) {
-    if(window.location.hash === `#${enSign}`) {
-        hideBg()
-    } else {
-        hideEn()
-    }
 }
 
-
-window.onresize = headerHeightWindow;
-window.onload = headerHeightWindow;
+window.onresize = headerHeightWindow
+window.onload = headerHeightWindow
 
 btns.forEach((btn) => {
     btn.addEventListener("click", () => {
-        if(btn.getAttribute("href") === `#${enSign}`) {
-            hideBg()
-        } else {
-            hideEn()
-        }
+        showSelect(btn.getAttribute("href"))
         setTimeout(() => {
-            if(btn.parentElement.className == "header__lang__dropdown") {
+            if(btn.parentElement.className == `${selectedArea}__lang__dropdown`) {
                 langDropdown.style.display = "none"
                 langBtnArrow.style.transform = "rotate(0deg)"
                 let btnTopA = langBtn.getElementsByTagName("a")[0]
@@ -57,6 +34,7 @@ btns.forEach((btn) => {
                 btn.href = `#${btnTopLink}`
                 btnTopA.getElementsByTagName("img")[0].src = `./pics/flag-${btnBotLink}.png`
                 btn.getElementsByTagName("img")[0].src = `./pics/flag-${btnTopLink}.png`
+                closeNav()
             }
         }, 50)
     })
@@ -72,9 +50,29 @@ langBtn.addEventListener("click", () => {
     }
 });
 
-langBtn.addEventListener("fade", () => {
-    setTimeout(() => {
-        langDropdown.style.display = "none"
-        langBtnArrow.style.transform = "rotate(0deg)"
-    }, 100)
-});
+if(window.location.hash) {
+    showSelect(window.location.hash)
+    btns.forEach((btn) => {
+        if(langBtn.getElementsByTagName("a")[0].getAttribute("href") !== window.location.hash) {
+            if(btn.getAttribute("href") === window.location.hash)  {
+                btn.click()
+            }
+        }
+    })
+}
+
+function openNav() {
+    document.getElementsByClassName("sidenav")[0].style.width = "calc(100vw - 30px)";
+    document.getElementsByClassName("header")[0].style.opacity = "0.6";
+    document.getElementsByClassName("main")[0].style.opacity = "0.6";
+    document.getElementsByClassName("footer")[0].style.opacity = "0.6";
+    document.getElementsByTagName("body")[0].style.backgroundColor = "#333"
+}
+  
+  function closeNav() {
+    document.getElementsByClassName("sidenav")[0].style.width = "0";
+    document.getElementsByClassName("header")[0].style.opacity = "1";
+    document.getElementsByClassName("main")[0].style.opacity = "1";
+    document.getElementsByClassName("footer")[0].style.opacity = "1";
+    document.getElementsByTagName("body")[0].style.backgroundColor = "#fff"
+  }
